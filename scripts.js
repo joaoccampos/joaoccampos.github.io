3cebf741c885d16dbb18500e41babddbd91cb7d3
JKiller13 <joaocorreiacampos@gmail.com>
2018-01-17 23:41:07 +0000

    prototipo final homepage

--- scripts.js
+++ scripts.js
@@ -1,8 +1,15 @@
-$(document).ready(() => {
-	
-    const $menuButton = $('.navbar-brand');
-    $menuButton.on('hover', () => {
-        $('.navbar').css('fontSize', '30px');
-    });
-	
+function goToByScroll(id){
+      // Remove "link" from the ID
+    id = id.replace("link", "");
+      // Scroll
+    $('html,body').animate({
+        scrollTop: $("#"+id).offset().top},
+        'slow');
+}
+
+$(".header-content-inner .container > a").click(function(e) { 
+      // Prevent a page reload when a link is pressed
+    e.preventDefault(); 
+      // Call the scroll function
+    goToByScroll(this.id);           
 });
